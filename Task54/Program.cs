30fc2a08e2eb721412870668682bc4f47ab9b7c6
Alex Belyaev <t433621@yandex.ru>
2022-08-20 09:42:27 +0300

    упорядочит по убыванию элементы каждой строки двумерного массивf

---
+++ Task54/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+В итоге получается вот такой массив:
+
+1 2 4 7
+
+2 3 5 9
+
+2 4 4 8 */
+
+Console.WriteLine("введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Sortirovka(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int q = 0; q < matr.GetLength(1) - 1; q++)
+            {
+                if (matr[i, q] > matr[i, q + 1])
+                {
+                    int temp = matr[i, q + 1];
+                    matr[i, q + 1] = matr[i, q];
+                    matr[i, q] = temp;
+                }
+            }
+        }
+    }
+}
+
+FillArray(array);
+Console.WriteLine("Выводим массив случайных чисел");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Упорядоченный массив:");
+Sortirovka(array);
+PrintArray(array);
